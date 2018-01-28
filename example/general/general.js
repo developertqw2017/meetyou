5e3d7af7934d408cb29ff10889561c7d82293b97
developertqw2017 <2144700613@qq.com>
2018-01-28 13:20:19 +0800

    oages

--- example/general/general.js
+++ example/general/general.js
@@ -1,107 +1,52 @@
 import weSwiper from '../../src/main'
 
-const option = {
-  touchstart (e) {
-    this.weswiper.touchstart(e)
+var util = require('../../util/util.js')
+
+const SAVE_VOICE = "保存录音"
+
+var playTimeInterval
+var recordTimeInterval
+
+Page({
+  onReady: function(e) {
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
   },
-  touchmove (e) {
-    this.weswiper.touchmove(e)
+  data: {
+    recording: false,
+    playing: false,
+    hasRecord: false,
+    recordTime: 0,
+    playTime: 0,
+    formatedRecordTime: '00:00:00',
+    formatedPlayTime: '00:00:00',
+    saveVoice: SAVE_VOICE
   },
-  touchend (e) {
-    this.weswiper.touchend(e)
-  },
-  onLoad () {
-    const device = wx.getSystemInfoSync()
-    new weSwiper({
-      animationViewName: 'animationData',
-      slideLength: 3,
-      initialSlide: 0,
-      /**
-       * swiper初始化后执行
-       * @param swiper
-       */
-      onInit (weswiper) {
-
-      },
-      /**
-       * 手指碰触slide时执行
-       * @param swiper
-       * @param event
-       */
-      onTouchStart (weswiper, event) {
-
-      },
-      /**
-       * 手指碰触slide并且滑动时执行
-       * @param swiper
-       * @param event
-       */
-      onTouchMove (weswiper, event) {
-
-      },
-      /**
-       * 手指离开slide时执行
-       * @param swiper
-       * @param event
-       */
-      onTouchEnd (weswiper, event) {
-
-      },
-      /**
-       *  slide达到过渡条件时执行
-       */
-      onSlideChangeStart (weswiper) {
-
-      },
-      /**
-       *  swiper从一个slide过渡到另一个slide结束时执行
-       */
-      onSlideChangeEnd (weswiper) {
-
-      },
-      /**
-       *  过渡时触发
-       */
-      onTransitionStart (weswiper) {
-
-      },
-      /**
-       *  过渡结束时执行
-       */
-      onTransitionEnd (weswiper) {
-
-      },
-      /**
-       *  手指触碰swiper并且拖动slide时执行
-       */
-      onSlideMove (weswiper) {
-
-      },
-      /**
-       * slide达到过渡条件 且规定了方向 向前（右、下）切换时执行
-       */
-      onSlideNextStart (weswiper) {
-
-      },
-      /**
-       *  slide达到过渡条件 且规定了方向 向前（右、下）切换结束时执行
-       */
-      onSlideNextEnd (weswiper) {
-
-      },
-      /**
-       *  slide达到过渡条件 且规定了方向 向前（左、上）切换时执行
-       */
-      onSlidePrevStart (swiper) {
-
-      },
-      /**
-       *  slide达到过渡条件 且规定了方向 向前（左、上）切换结束时执行
-       */
-      onSlidePrevEnd (weswiper) {
-
-      }
+  startRecord: function() {
+    this.setData({
+      recording: true
     })
-  }
-}
-Page(option)
+
+    var that = this
+    recordTimeInterval = setInterval(function() {
+      var recordTime = that.data.recordTime += 1
+      that.setData({
+        formatedRecordTime: util.formatTime(that.data.recordTime),
+        recordTime: recordTime
+      })
+    }, 1000)
+
+    wx.startRecord({
+      success: function(res) {
+
+        // @ tempFilePath: wxfile://    本地临时录音的路径
+        // @ errMsg:  startRecord : ok  应该是返回信息
+        that.setData({
+          hasRecord: true,
+          tempFilePath: res.tempFilePath,
+          formatedPlayTime: util.formatTime(that.data.playTime)
+        })
+      },
+      complete: function() {
+        that.setData({
+          recording: false
